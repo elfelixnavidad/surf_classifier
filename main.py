34b2dc6337e18b5dc295d1a2c196ef562783f77a
Felix Sanchez <felix@Felixs-MBP.attlocal.net>
2023-04-01 20:07:10 -0700

    first commit; dummy data

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+import json
+
+def main():
+    with open('wave.json', 'r') as f:
+        data = json.load(f)
+
+    df_list = []
+
+    col_names = [
+        'timestamp_epoch',
+        'surf_min',
+        'surf_max',
+        'surf_humanRelation',
+    ]
+
+    for i in range(6):
+        col_names.append(f'swell_{i}_height')
+        col_names.append(f'swell_{i}_period')
+        col_names.append(f'swell_{i}_impact')
+        col_names.append(f'swell_{i}_direction')
+        col_names.append(f'swell_{i}_directionMin')
+        col_names.append(f'swell_{i}_optimalScore')
+        
+    for d in data['data']['wave']:
+        ts = int(d['timestamp'])
+        surf_min = float(d['surf']['raw']['min'])
+        surf_max = float(d['surf']['raw']['max'])
+        surf_humanRelation = d['surf']['humanRelation']
+
+        swell_list = list(d['swells'])
+        swell_vector = list()
+        
+        # Only ever see 6 diff swells so we working on that assumption
+        for i in range(6):
+            swell_vector.append(np.round(float(swell_list[i]['height']), 2))
+            swell_vector.append(int(swell_list[i]['period']))
+            swell_vector.append(np.round(float(swell_list[i]['impact']), 2))
+            swell_vector.append(np.round(float(swell_list[i]['direction']), 2))
+            swell_vector.append(np.round(float(swell_list[i]['directionMin']), 2))
+            swell_vector.append(int(swell_list[i]['optimalScore']))
+
+        df_list.append(pd.DataFrame(data=[[ts, surf_min, surf_max, surf_humanRelation, *swell_vector]], columns=col_names))
+
+
+    wave_df = pd.concat(df_list)
+    wave_df['timestamp_utc'] = pd.to_datetime(wave_df['timestamp_epoch'], unit='s')
+    wave_df = wave_df.set_index('timestamp_epoch')
+    wave_df['rating'] = ''
+    
+    for i in wave_df.index:
+        if wave_df.loc[i, 'surf_max'] >= 2:
+            wave_df.loc[i, 'rating'] = 'N'
+        else:
+            wave_df.loc[i, 'rating'] = 'Y'
+
+    wave_df.to_csv('wave_dummy_data.tsv', sep='\t')
+if __name__ == '__main__':
+    main()
+
